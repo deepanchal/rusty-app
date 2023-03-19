c85cfb11ecd11c3fd4ceb9721266fdecb189c4b7
Deep Panchal <deep302001@gmail.com>
2023-03-19 14:23:10 -0500

    feat(server): cleanup main fn, format code

--- crates/server/src/bin/server.rs
+++ crates/server/src/bin/server.rs
@@ -1,21 +1,8 @@
 use axum::{extract::Query, response::Html, routing::get, Router};
 use rand::{thread_rng, Rng};
 use serde::Deserialize;
-use tokio::signal;
 use std::net::SocketAddr;
-
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(handler));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3333));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
-}
+use tokio::signal;
 
 // `Deserialize` need be implemented to use with `Query` extractor.
 #[derive(Deserialize)]
@@ -24,12 +11,28 @@ struct RangeParameters {
     end: usize,
 }
 
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+    serve(app, 3333).await;
+}
+
 async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
     // Generate a random number in range parsed from query.
     let random_number = thread_rng().gen_range(range.start..range.end);
 
     // Send response in html format.
     Html(format!("<h1>Random Number: {}</h1>", random_number))
+}
+
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
 async fn shutdown_signal() {
@@ -57,4 +60,3 @@ async fn shutdown_signal() {
 
     println!("signal received, starting graceful shutdown");
 }
-
