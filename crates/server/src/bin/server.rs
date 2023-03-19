54890695dd5cd37b72feabda76564bea20854d41
Deep Panchal <deep302001@gmail.com>
2023-03-19 14:16:20 -0500

    feat(server): use graceful shutdown for axum server

--- crates/server/src/bin/server.rs
+++ crates/server/src/bin/server.rs
@@ -1,6 +1,7 @@
 use axum::{extract::Query, response::Html, routing::get, Router};
 use rand::{thread_rng, Rng};
 use serde::Deserialize;
+use tokio::signal;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -11,6 +12,7 @@ async fn main() {
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
@@ -29,3 +31,30 @@ async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
     // Send response in html format.
     Html(format!("<h1>Random Number: {}</h1>", random_number))
 }
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
+}
+
