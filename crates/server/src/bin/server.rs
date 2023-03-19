d30e09fa15d4e3220c79626a1edafa958ed89ba8
Deep Panchal <deep302001@gmail.com>
2023-03-19 15:16:16 -0500

    feat(server): add simple CRUD todos from axum examples in server.rs

--- crates/server/src/bin/server.rs
+++ crates/server/src/bin/server.rs
@@ -1,14 +1,52 @@
 use axum::{
-    extract::Query,
+    error_handling::HandleErrorLayer,
+    extract::{Path, Query, State},
     response::{Html, IntoResponse},
-    routing::get,
-    Router,
+    routing::{get, patch},
+    Json, Router,
 };
 use hyper::StatusCode;
 use rand::{thread_rng, Rng};
-use serde::Deserialize;
-use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
 use tokio::signal;
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
+
+// Temp db
+type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
+
+#[derive(Debug, Serialize, Clone)]
+struct Todo {
+    id: Uuid,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct TodoCreate {
+    text: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct TodoUpdate {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+// The query parameters for todos index
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
 
 // `Deserialize` need be implemented to use with `Query` extractor.
 #[derive(Deserialize)]
@@ -19,8 +57,39 @@ struct RangeParameters {
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let db = Db::default();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_todos=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with routes
+    let app = Router::new()
+        .route("/", get(handler_rand_num))
+        .route("/todos", get(todos_index).post(todos_create))
+        .route("/todos/:id", patch(todos_update).delete(todos_delete))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
+        .with_state(db);
 
     // add a fallback service for handling routes to unknown paths
     let app = app.fallback(handler_404);
@@ -28,7 +97,7 @@ async fn main() {
     serve(app, 3333).await;
 }
 
-async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
+async fn handler_rand_num(Query(range): Query<RangeParameters>) -> Html<String> {
     // Generate a random number in range parsed from query.
     let random_number = thread_rng().gen_range(range.start..range.end);
 
@@ -74,4 +143,67 @@ async fn shutdown_signal() {
     }
 
     println!("signal received, starting graceful shutdown");
+}
+
+async fn todos_index(
+    pagination: Option<Query<Pagination>>,
+    State(db): State<Db>,
+) -> impl IntoResponse {
+    let todos = db.read().unwrap();
+
+    let Query(pagination) = pagination.unwrap_or_default();
+
+    let todos = todos
+        .values()
+        .skip(pagination.offset.unwrap_or(0))
+        .take(pagination.limit.unwrap_or(usize::MAX))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    Json(todos)
+}
+
+async fn todos_create(State(db): State<Db>, Json(input): Json<TodoCreate>) -> impl IntoResponse {
+    let todo = Todo {
+        id: Uuid::new_v4(),
+        text: input.text,
+        completed: false,
+    };
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+async fn todos_update(
+    Path(id): Path<Uuid>,
+    State(db): State<Db>,
+    Json(input): Json<TodoUpdate>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let mut todo = db
+        .read()
+        .unwrap()
+        .get(&id)
+        .cloned()
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    if let Some(text) = input.text {
+        todo.text = text;
+    }
+
+    if let Some(completed) = input.completed {
+        todo.completed = completed;
+    }
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+
+    Ok(Json(todo))
+}
+
+async fn todos_delete(Path(id): Path<Uuid>, State(db): State<Db>) -> impl IntoResponse {
+    if db.write().unwrap().remove(&id).is_some() {
+        StatusCode::NO_CONTENT
+    } else {
+        StatusCode::NOT_FOUND
+    }
 }
